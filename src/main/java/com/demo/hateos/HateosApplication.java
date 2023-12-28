de4e891c22918b65c4a19501645778625bd30206
massimoesposito <massimo.esposito@conversocial.com>
2023-12-29 00:35:16 +0100

    remove unused import

--- src/main/java/com/demo/hateos/HateosApplication.java
+++ src/main/java/com/demo/hateos/HateosApplication.java
@@ -2,8 +2,6 @@ package com.demo.hateos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.web.PagedResourcesAssembler;
 
 @SpringBootApplication
 public class HateosApplication {
